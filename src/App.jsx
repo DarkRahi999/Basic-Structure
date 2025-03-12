031d711bce4822714a6521056651d62e01abb6c6
Bisakto_Rahi <bisaktorahi007@gmail.com>
2025-03-12 22:27:34 +0600

    all set

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Body} from './components'
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <Body></Body>
+    </>
+  )
+}
+export default App
---
+++ src/components.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+export function Body(){
+    return(
+        <div className="div">
+            <h2>Set up Ready</h2>
+            <h2>all ok</h2>
+        </div>
+    )
+}
