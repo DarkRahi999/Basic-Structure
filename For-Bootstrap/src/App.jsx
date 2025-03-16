6af1ff9ae93ad3d2ae488dd097567816713d64a2
Bisakto_Rahi <bisaktorahi007@gmail.com>
2025-03-16 20:02:08 +0600

    comments done

--- For-Bootstrap/src/App.jsx
+++ For-Bootstrap/src/App.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
-import {Body} from './components';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
+import React from 'react'                             // Y_---------{ React file  }----------//
+import {Body} from './components';                    // Y_---------{ For Component File   }----------//
+import 'bootstrap/dist/css/bootstrap.min.css';        // Y_---------{ Bootstrap 5 CSS file  }---------//
+import './App.css';                                   //Y_----------{ For Customize Style  }----------//
+import './rwd.css';                                   // Y_---------{ For Responsive Style  }---------//
 function App() {
   return (
     <>
--- For-Bootstrap/src/components.jsx
+++ For-Bootstrap/src/components.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import { Button } from 'react-bootstrap';
-import { FaCheckCircle } from "react-icons/fa";
+import React from 'react'                                       // Y_---------{ React file  }----------//
+import { Button } from 'react-bootstrap';                       // Y_---------{ Bootstrap cdn  }----------//
+import { FaCheckCircle } from "react-icons/fa";                 // Y_---------{ FontAwesome cdn }----------//
 export function Body(){
     return(
         <div className="div">
