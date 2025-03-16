ad583caa1cae9c65ae6525c73de42df8d6691714
Bisakto_Rahi <bisaktorahi007@gmail.com>
2025-03-16 21:49:20 +0600

    just add tailwind folder

---
+++ For-Tailwind/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'                             // Y_---------{ React file  }----------//
+import {Body} from './components';                    // Y_---------{ For Component File   }----------//
+import './App.css';                                   //Y_----------{ For Customize Style  }----------//
+import './rwd.css';                                   // Y_---------{ For Responsive Style  }---------//
+function App() {
+  return (
+    <>
+    <Body />
+    </>
+  )
+}
+export default App
---
+++ For-Tailwind/src/components.jsx
@@ -0,0 +1,11 @@
+import React from 'react'                                       // Y_---------{ React file  }----------//
+import { FaCheckCircle } from "react-icons/fa";                 // Y_---------{ FontAwesome cdn }----------//
+export function Body(){
+    return(
+        <div className="div">
+            <h2>Set up Ready</h2>
+            <h2>all ok <br />
+            <FaCheckCircle className='todoICON'/></h2>
+        </div>
+    )
+}
